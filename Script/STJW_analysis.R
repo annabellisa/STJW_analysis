95aedbdd4778fcd02c62c47ef330b1903b30260e
Emily Hedger <emilyhedger@Emilys-MacBook-Pro.local>
2020-07-30 16:50:29 +1000

    Saving console(?) work

--- Script/STJW_analysis.R
+++ Script/STJW_analysis.R
@@ -6,9 +6,15 @@ dir("Data")
 
 pn<-read.table("Data/Plant_info.txt",header=T)
 s17<-read.table("Data/Survey_data_observed_sp_2017.txt",header=F)
+s18<-read.table("Data/Survey_data_observed_sp_2018.txt",header=F)
+s19<-read.table("Data/Survey_data_observed_sp_2019.txt",header=F)
+
 
 head(pn); dim(pn)
 head(s17[,1:10],8); dim(s17)
+head(s18[,1:10],8); dim(s18)
+head(s19[,1:10],8); dim(s19)
+
 
 
 
