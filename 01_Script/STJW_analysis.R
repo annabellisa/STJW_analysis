b7e409a75eef9b779a8d75ee7f6100e59e6475ca
Annabel Smith <annabel@smithecology.org>
2020-12-03 08:25:50 +1000

    AS updates

--- 01_Script/STJW_analysis.R
+++ 01_Script/STJW_analysis.R
@@ -1,3 +1,16 @@
+
+# ------------------------------------ #
+# ---------- STJW ANALYSIS  ---------- #
+# ------------------------------------ #
+
+### Analysis of chemical control experiment from STJW project
+### Author: Annabel Smith & Raagini Muddaiah
+
+# load functions:
+invisible(lapply(paste("01_Functions/",dir("01_Functions"),sep=""),function(x) source(x)))
+
+
+
 # updating script
 
 Data_dir<-"Data/Final_datasets/Formatted_data"
@@ -54,52 +67,6 @@ LETTERS[23:25]
 length(which(ct17d[,1]%in%LETTERS [23:25])) #which has letters in them
 apply(ct17d,2,function(x)length(which(x%in%LETTERS [23:25]))) #which sp. has letters like W and need to be replaced
 
-
-
-
-
-
-#old stuff:
-
-pn<-read.table("Data/Plant_info.txt",header=T)
-s17<-read.table("Data/Survey_data_observed_sp_2017.txt",header=F)
-s18<-read.table("Data/Survey_data_observed_sp_2018.txt",header=F)
-s19<-read.table("Data/Survey_data_observed_sp_2019.txt",header=F)
-d1<-read.table("Data/DAT.txt",header=T)
-
-head(pn); dim(pn)
-head(s17[,1:10],8); dim(s17)
-head(s18[,1:10],8); dim(s18)
-head(s19[,1:10],8); dim(s19)
-head(c17[,1:10],8); dim(c17)
-head(c18[,1:10],8); dim(c18)
-head(c19[,1:10],8); dim(c19)
-
-
-head(count17[,1:6])
-dir("Data")
-
-pdat<-read.table("Data/Plant_info.txt", header=T)
-head(pdat)
-
-cnames<-colnames(count17[,which(colnames(count17)=="Air_sp"):ncol(count17)])
-head(cnames)
-length(cnames)
-
-head(pdat, 3)
-head(count17[,1:6])
-
-# you want this line to give you nothing:
-which(!cnames %in% pdat$Sp)
-
-which(pdat$Sp=="Air_sp")
-dim(count17)
-311+6
-
-head(count17[,1:10],3)
-count17[,length(count17)]
-
-colSums(count17[,which(colnames(count17)=="Air_sp"):ncol(count17)])
 
 
 
--- 01_Script/STJW_analysis_development.R
+++ 01_Script/STJW_analysis_development.R
@@ -29,6 +29,52 @@ which(pnex$species %in% pn2$species)
 
 
 
+#old stuff:
+
+pn<-read.table("Data/Plant_info.txt",header=T)
+s17<-read.table("Data/Survey_data_observed_sp_2017.txt",header=F)
+s18<-read.table("Data/Survey_data_observed_sp_2018.txt",header=F)
+s19<-read.table("Data/Survey_data_observed_sp_2019.txt",header=F)
+d1<-read.table("Data/DAT.txt",header=T)
+
+head(pn); dim(pn)
+head(s17[,1:10],8); dim(s17)
+head(s18[,1:10],8); dim(s18)
+head(s19[,1:10],8); dim(s19)
+head(c17[,1:10],8); dim(c17)
+head(c18[,1:10],8); dim(c18)
+head(c19[,1:10],8); dim(c19)
+
+
+head(count17[,1:6])
+dir("Data")
+
+pdat<-read.table("Data/Plant_info.txt", header=T)
+head(pdat)
+
+cnames<-colnames(count17[,which(colnames(count17)=="Air_sp"):ncol(count17)])
+head(cnames)
+length(cnames)
+
+head(pdat, 3)
+head(count17[,1:6])
+
+# you want this line to give you nothing:
+which(!cnames %in% pdat$Sp)
+
+which(pdat$Sp=="Air_sp")
+dim(count17)
+311+6
+
+head(count17[,1:10],3)
+count17[,length(count17)]
+
+colSums(count17[,which(colnames(count17)=="Air_sp"):ncol(count17)])
+
+
+
+
+
 
 
 
